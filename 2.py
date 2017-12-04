2405698867bff4a7100b98dfd2154243d53f1f35
lionasp <lion.programmer@gmail.com>
2017-12-04 09:08:23 +0300

    update 2 issue

--- 2.py
+++ 2.py
@@ -1,6 +1,17 @@
 with open('2.in') as f:
     diffs = []
+    dividers = []
     for line in f.readlines():
         numbers = [int(i) for i in line.split()]
         diffs.append(max(numbers) - min(numbers))
+        br = 0
+        for i in numbers:
+            for j in numbers:
+                if i != j and i % j == 0:
+                    dividers.append(i // j)
+                    br = 1
+                    break
+            if br:
+                break
     print(sum(diffs))
+    print(sum(dividers))
