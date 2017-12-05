9683cbcd3fa0ef855f82a92c072fbd9c81025aa8
lionasp <lion.programmer@gmail.com>
2017-12-05 09:33:15 +0300

    update 5th issue (add second part)

--- 5.py
+++ 5.py
@@ -1,6 +1,6 @@
 with open('5.in') as f:
-    line = f.readlines()
-line = [int(i) for i in line]
+    raw_line = f.readlines()
+line = [int(i) for i in raw_line]
 cursor = 0
 l = len(line)
 jumps = 0
@@ -8,6 +8,18 @@ jumps = 0
 while cursor < l:
     next_jump_size = line[cursor]
     line[cursor] += 1
+    cursor += next_jump_size
+    jumps += 1
+
+print(jumps)
+
+line = [int(i) for i in raw_line]
+cursor = 0
+jumps = 0
+# second part
+while cursor < l:
+    next_jump_size = line[cursor]
+    line[cursor] += 1 if next_jump_size < 3 else -1
     cursor += next_jump_size
     jumps += 1
 
