9a14b6b8da59102b1b5a2c2fa0db9e703dab6353
lionasp <lion.programmer@gmail.com>
2017-12-17 10:20:44 +0300

    issue 15

--- 15.py
+++ 15.py
@@ -6,10 +6,25 @@ to = 40000000
 for i in range(to):
     a = (a * 16807) % 2147483647
     b = (b * 48271) % 2147483647
-    if i % 100000 == 0:
-        print(i)
-    # print(str(bin(a))[-16:])
-    # print(str(bin(b))[-16:])
+    if str(bin(a))[-16:] == str(bin(b))[-16:]:
+        judge += 1
+print(judge)
+
+# part 2
+a = 883
+b = 879
+
+judge = 0
+to = 5000000
+for i in range(to):
+    while True:
+        a = (a * 16807) % 2147483647
+        if a % 4 == 0:
+            break
+    while True:
+        b = (b * 48271) % 2147483647
+        if b % 8 == 0:
+            break
     if str(bin(a))[-16:] == str(bin(b))[-16:]:
         judge += 1
 print(judge)
