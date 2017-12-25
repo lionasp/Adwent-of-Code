b7402ef9f8e94df74b4b3eba4efcd1af7c70b49b
lionasp <lion.programmer@gmail.com>
2017-12-25 14:37:59 +0300

    issue 20 part 2

--- 20.py
+++ 20.py
@@ -1,3 +1,5 @@
+from copy import deepcopy
+
 commands = {}
 with open('20.in') as f:
     n = 0
@@ -8,6 +10,7 @@ with open('20.in') as f:
                        'a': [int(i) for i in split[2][3:-1].split(',')]}
         n += 1
 
+commands2 = deepcopy(commands)
 
 for i in range(10 ** 3):
     for n in commands:
@@ -23,5 +26,45 @@ for n in commands:
         min_distance = check
         index = n
 
-print(min_distance)
 print(index)
+
+
+# part 2
+l_commands = len(commands2)
+destroy_count = 0
+def check_destroy():
+    global destroy_count
+    cp = commands2.copy()
+    for c in cp:
+        founded = False
+        try:
+            p = commands2[c]['p']
+        except:
+            continue
+        for j in cp:
+            if j == c:
+                continue
+            try:
+                commands2[j]
+            except:
+                continue
+            jp = commands2[j]['p']
+            if p[0] == jp[0] and p[1] == jp[1] and p[2] == jp[2]:
+                del commands2[j]
+                destroy_count += 1
+                founded = True
+
+        if founded and c in commands2:
+            del commands2[c]
+            destroy_count += 1
+
+
+check_destroy()
+for i in range(100):
+    for n in commands2:
+        for m in range(3):
+            commands2[n]['v'][m] += commands2[n]['a'][m]
+            commands2[n]['p'][m] += commands2[n]['v'][m]
+    check_destroy()
+
+print(l_commands - destroy_count)
